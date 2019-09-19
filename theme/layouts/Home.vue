25ddefad526fc636880f377857352519b1020b74
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2019-09-19 01:41:11 +0100

    work in progress

--- theme/layouts/Home.vue
+++ theme/layouts/Home.vue
@@ -1,24 +1,21 @@
 <template>
-  <Wrap>
-    <h1>{{ page.title }}</h1>
-    <h1>Here might be a page title</h1>
-    <p>A paragraph for the main content.</p>
-    <p>And another one.</p>
-  </Wrap>
+	<Wrap>
+		<slot />
+	</Wrap>
 </template>
 
 <script>
-import Wrap from "@/theme/components/Wrap.vue";
-import page from "@/pages/home/home.yml";
-import features from "@/pages/home/features.yml";
+import Wrap from '@/theme/components/Wrap.vue'
+import page from '@/pages/home/home.yml'
+import features from '@/pages/home/features.yml'
 
 export default {
-  components: { Wrap },
-  data() {
-    return {
-      page,
-      features
-    };
-  }
-};
+	components: { Wrap },
+	data() {
+		return {
+			page,
+			features
+		}
+	}
+}
 </script>
--- theme/layouts/cv.vue
+++ theme/layouts/cv.vue
@@ -1,34 +1,49 @@
 <template>
-  <Wrap>
-    <Section v-for="section in sections" :key="section" class="bt">
-      <h2 class="md:w-200 f-caps f-0">{{ section.heading }}</h2>
-      <div v-html="markdown(section.content)"></div>
-    </Section>
-    <slot />
-  </Wrap>
+	<Wrap>
+		<Section
+			v-for="(section, index) in sections"
+			:key="index"
+			class="bt"
+			:class="kebabcase(section.heading)"
+		>
+			<h2 class="md:w-200 f-caps f-0">{{ section.heading }}</h2>
+			<div class="content" v-html="markdown(section.content)"></div>
+		</Section>
+		<slot />
+	</Wrap>
 </template>
 
 <script>
-import Wrap from "@/theme/components/Wrap.vue";
-import Section from "@/theme/components/Section.vue";
-import page from "@/pages/home/home.yml";
-import sections from "@/pages/cv/sections.yml";
-import marked from "marked";
-import MarkdownIt from "markdown-it";
-var md = new MarkdownIt();
+import Wrap from '@/theme/components/Wrap.vue'
+import Section from '@/theme/components/Section.vue'
+import page from '@/pages/home/home.yml'
+import sections from '@/pages/cv/sections.yml'
+import MarkdownIt from 'markdown-it'
+import mdAttrs from 'markdown-it-attrs'
+import v from 'voca'
+var md = new MarkdownIt({ html: true })
+md.use(mdAttrs)
 
 export default {
-  components: { Wrap, Section },
-  data() {
-    return {
-      page,
-      sections
-    };
-  },
-  methods: {
-    markdown: function(input) {
-      return md.render(input);
-    }
-  }
-};
+	components: { Wrap, Section },
+	data() {
+		return {
+			page,
+			sections
+		}
+	},
+	methods: {
+		markdown: function(input) {
+			return md.render(input)
+		},
+		kebabcase(input) {
+			return v.kebabCase(input)
+		},
+		hasColumns(input) {
+			if (this.kebabcase(input.heading) === 'highlights') {
+				return 'md:cols-2'
+			}
+		}
+	}
+}
 </script>
