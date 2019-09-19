e31ce3f7eaec93f56a68548c10852ec18bec5928
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2019-09-19 17:24:25 +0100

    work in progress

--- .saber/layouts.js
+++ .saber/layouts.js
@@ -1,20 +1,20 @@
 var layouts = {}
 
     
-      import layout_0 from "/Users/limitlessloop/Repos/personal-site-saber/theme/layouts/cv.vue"
-      layouts["cv"] = layout_0
+      import layout_0 from "/Users/limitlessloop/Repos/personal-site-saber/theme/layouts/default.vue"
+      layouts["default"] = layout_0
       
 
-      import layout_1 from "/Users/limitlessloop/Repos/personal-site-saber/theme/layouts/default.vue"
-      layouts["default"] = layout_1
+      import layout_1 from "/Users/limitlessloop/Repos/personal-site-saber/theme/layouts/cv.vue"
+      layouts["cv"] = layout_1
       
 
-      import layout_2 from "/Users/limitlessloop/Repos/personal-site-saber/theme/layouts/home.vue"
-      layouts["home"] = layout_2
+      import layout_2 from "/Users/limitlessloop/Repos/personal-site-saber/theme/layouts/posts.vue"
+      layouts["posts"] = layout_2
       
 
-      import layout_3 from "/Users/limitlessloop/Repos/personal-site-saber/theme/layouts/posts.vue"
-      layouts["posts"] = layout_3
+      import layout_3 from "/Users/limitlessloop/Repos/personal-site-saber/theme/layouts/home.vue"
+      layouts["home"] = layout_3
       
 
     export default layouts
